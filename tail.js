0683752f812acdc3bb5085f922a8ac17e40bca9b
Taomosquito <S_Peloquin123@live.com>
2024-05-02 01:05:25 -0400

    added the necessary testing steps

--- tail.js
+++ tail.js
@@ -1,3 +1,12 @@
+//Borrowed from assertEqual.js and a better means of accessing it will be available later.
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 // This function drops off the first element of the array than sends back a new array in its place.
 const tail = function(arr) {
   let arr2 = [];
@@ -6,4 +15,12 @@ const tail = function(arr) {
   }
   return arr2;
 };
+
+let sampleArray = [1,2,3,4,5];
 console.log(tail([1]));
+console.log(tail([]));
+console.log(tail([1,2,3,4,5]));
+console.assert(tail(sampleArray).length === sampleArray.length - 1);
+console.log(tail(sampleArray));
+console.log(sampleArray);
+assertEqual(tail(sampleArray)[0], sampleArray[1]);
