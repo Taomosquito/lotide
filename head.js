6b534fdabb8064ac97aa3713ebdfd0cd69a416fd
Taomosquito <S_Peloquin123@live.com>
2024-05-02 00:07:15 -0400

    updated to have comments and ran linter

--- head.js
+++ head.js
@@ -1,3 +1,4 @@
+// borrows a function from another file which later can be done using more streamlined means.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,9 +7,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(arr){
+// returns the first element in an array.
+const head = function(arr) {
   return arr[0];
-}
+};
 assertEqual(head([5,6,7]),5);
 assertEqual(head(["Hello","Lighthouse","Labs"]), "Hello");
 assertEqual(head([]), undefined);
