b6110a1e37286306fb3e825374727eedf9b900b6
Taomosquito <S_Peloquin123@live.com>
2024-05-02 23:18:32 -0400

    built a without function that creates a new array with only the values in both arrays!

---
+++ without.js
@@ -0,0 +1,51 @@
+// Established a testing array so that I can show it remains unchanged
+let testingArrayA = [1,2,3];
+let testingArrayB = [1,2];
+
+// A function checking if two arrays are equal for testing purposes.
+const eqArrays = function(arrA, arrB) {
+  let test = true;
+  if (arrA.length !== arrB.length) {
+    test = false;
+  } else {
+    for (let i = 0; i < arrA.length; i++) {
+      if (arrA[i] !== arrB[i]) {
+        test = false;
+        break;
+      }
+    }
+  }
+  return test;
+};
+
+// A function providing an assertion message success or passed for the above.
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// Newly formed function that removes anything making arays unequal in the new value it returns.
+const without = function(source, itemsToRemove){
+  let result = []
+  for(i = 0; i < source.length; i++){
+    if(source[i] === itemsToRemove[i]){
+      result.push(source[i]);
+    }
+  }
+  console.log(result);
+  return result;
+}
+// Testing Conditions
+
+assertArraysEqual([1,2,3],[1,2,3]);
+without(["1","2","3"],["1","2","3"]);
+assertArraysEqual(["1","2","3"],["1","2","3"]);
+without(["1","2","3"],["1","2", 3]);
+assertArraysEqual(["1","2","3"],["1","2",3]);
+assertArraysEqual(["1","2","3"],["1","2"]);
+without(["1","2","3"],["1","2"]);
+without(testingArrayA, testingArrayB);
+console.log(testingArrayA);
+console.log(testingArrayB);
