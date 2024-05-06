0186f895a6cea195d41656cf78de96f0fce3e96d
Taomosquito <S_Peloquin123@live.com>
2024-05-05 22:27:20 -0400

    started and finished the letterPosition functionality

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+// preconfigured requirement for the testing function.
+const eqArrays = function (arrA, arrB) {
+  let test = true;
+  if (arrA.length !== arrB.length) {
+    test = false;
+  } else {
+    for (let i = 0; i < arrA.length; i++) {
+      if (arrA[i] !== arrB[i]) {
+        test = false;
+        break;
+      }
+    }
+  }
+  return test;
+};
+
+// preconfigured testing function.
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// establishes a function that counts letter positions using a multi layer nested object.
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+// tests.
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);  
