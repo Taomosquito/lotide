47d7168f53977b43df884f3a20258ec11e2027cb
Taomosquito <S_Peloquin123@live.com>
2024-05-10 23:17:52 -0400

    updated the assertion failed message to be more descriptive

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -41,7 +41,7 @@ const assertObjectsEqual = function(actual, expected) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   }
   if (!eqObjects(actual, expected)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 assertObjectsEqual({ a: 1, b: 2 }, { b: 2, a: 1 }); 
