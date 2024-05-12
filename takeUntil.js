a240bb6122d4aaea6b9fea58ab38ce84140f7bf4
Taomosquito <S_Peloquin123@live.com>
2024-05-11 21:38:18 -0400

    optimized to avoid suboptimal code issues.

--- takeUntil.js
+++ takeUntil.js
@@ -22,7 +22,7 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const takeUntil = function(array, callback) {
-  let = newArray = [];
+  let newArray = [];
   for (let i = 0; i < array.length; i++) {
     let element = array[i];
     newArray.push(element);
